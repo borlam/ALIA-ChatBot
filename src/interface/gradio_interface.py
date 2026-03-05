1ba68c34fc64ced591023a2e284adab9ac2bdc94
Jorge Aparicio Bermejo <jorge.aparicio-bermejo@hp.com>
2026-03-05 17:35:02 +0100

    Remove messages in the ChatBot

--- src/interface/gradio_interface.py
+++ src/interface/gradio_interface.py
@@ -55,8 +55,7 @@ class GradioInterface:
                     # Chatbot
                     chatbot = gr.Chatbot(
                         label="💬 Conversación Inteligente",
-                        height=500,
-                        type="messages"
+                        height=500
                     )
                     
                     # Área de entrada mejorada
